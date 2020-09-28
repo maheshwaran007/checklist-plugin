da85eace37de812bd1352fb944e6a7f222a2d631
maheshwaran007 <71010639+maheshwaran007@users.noreply.github.com>
2020-09-29 00:15:49 +0530

    last change of parent shortcode.
    last change of parent shortcode

--- checkboxfunction.php
+++ checkboxfunction.php
@@ -8,31 +8,41 @@ function custom_checkbox($atts,$content = null){
 	), $atts );	
 	if ( has_shortcode( $content, 'button' ) ) {
 		// The content has a short code, so this check returned true.		
-		$sub_shortcode_parts = preg_split("/\[button /i", $content);
+		$sub_shortcode_parts = preg_split("/\[button /i", $content);		
 		$sub_shortcode_array = array();
+		$sub_arr_name_cur = array();
+		$page_id = get_the_ID();
+		 
 		foreach($sub_shortcode_parts as $sub_shortcode){
 			if($sub_shortcode !='' && !empty($sub_shortcode)){
 				$sub_shortcode_array[] = $sub_shortcode;
+				$sub_shortcode_arr_name = explode("name=", $sub_shortcode);
+				  $sub_arr_name = explode("]",$sub_shortcode_arr_name[1]);
+				  $sub_arr_name_cur[] = isset($sub_arr_name['0'])?$sub_arr_name['0']:'';
 			}	
-		}			
+		}	
+		$sub_arr_name_unigue = array_unique($sub_arr_name_cur);
+		
+        $sub_arr_name_duplicate = array_diff_assoc($sub_arr_name_cur, $sub_arr_name_unigue);		
+		
 		$sub_shortcode_str = '';
 		$checkbox_attr_str = '';
-		foreach($sub_shortcode_array as $sub_shortcode_arrays){	 
-		   
-	       if($checkbox_attr['name'] !=''){
-			  $page_id = get_the_ID();
-			  //checkbox name checked with master table
-			  echo $checkboxname_result = checkboxname_check($checkbox_attr["name"],$page_id);
-			  if($checkboxname_result == 'new'){
-			      $sub_shortcode_str = '[button checkboxname="'.$checkbox_attr["name"].'" '.$sub_shortcode_arrays;
-			      $checkbox_attr_str .= do_shortcode($sub_shortcode_str);
-			  }else{
-				$checkbox_attr_str = '<p class="error_msg">Checkbox name already exit.<p>';  
-			  } 
+		$i = 0;
+		
+		foreach($sub_arr_name_unigue as $sub_shortcode_arrays){	 		   
+	       if($checkbox_attr['name'] !=''){	
+	           if($sub_shortcode_arrays){		  
+			       $sub_shortcode_str = '[button checkboxname="'.$checkbox_attr["name"].'"  name="'.$sub_shortcode_arrays.'"]';		 
+			        $checkbox_attr_str .= do_shortcode($sub_shortcode_str);	
+			   }   
+			   
 	       }else{
 			  $checkbox_attr_str = '<p class="error_msg">Please add checkbox shortcode name Attribute.<p>';
 		   }   
-		} 
+		}
+	    if(!empty($sub_arr_name_duplicate) && $checkbox_attr['name'] !=''){	
+		    $checkbox_attr_str .= '<p class="error_msg">button name already exit are '.implode(",",$sub_arr_name_duplicate).'<p>';  	
+		}		
     }
 			  
 	return $checkbox_attr_str;
@@ -45,22 +55,29 @@ add_shortcode('customcheckbox', 'custom_checkbox');
  function custom_checkbox_button($atts,$content = null){
 	
 	$checkbox_button_attr = shortcode_atts( array(
-		'name' => '',
-		'checkboxname' => '',
+		"name" => "",
+		"checkboxname" => "",
 	), $atts );
+	$count = 1;
 	$checkbox_button_attr_str = '';	
-	if($checkbox_button_attr['name'] !='' && $checkbox_button_attr['checkboxname']){
-		$checkboxid = $checkbox_button_attr['checkboxname']."_".$checkbox_button_attr['name'];
+	$checkbox_button_name = str_replace("'", '', $checkbox_button_attr['name']);
+	$checkbox_name = str_replace("'", '', $checkbox_button_attr['checkboxname']);	
+	if($checkbox_button_name !='' && $checkbox_name){
+		$checkboxid = trim($checkbox_name."_".$checkbox_button_name);
 		$page_id = get_the_ID();
 		$ajax_url = admin_url( 'admin-ajax.php' );
 		$checkboxfunarg  = '"'.$checkboxid.'","'.$ajax_url.'","'.$page_id.'"'; 
 		$usert_id = get_current_user_id();
 		$selected_value = checkbox_select_display($page_id,$checkboxid,$usert_id);
-	    $checkbox_button_attr_str = "<label class='checkboxlabel' for='".$checkboxid."' onchange='checkboxevent(".$checkboxfunarg.")'>";
-		$checkbox_button_attr_str .= "<input type='checkbox' id='".$checkboxid."' name='".$checkboxid."' value='".$checkbox_button_attr['name']."' ".$selected_value.">".$checkbox_button_attr['name']."</input><span class='checkmarkspan'></span></label>";	
+		$checkbox_button_attr_str = "<label class='checkboxlabel' for='".$checkboxid."' onchange='checkboxevent(".$checkboxfunarg.")'>";
+	 	$checkbox_button_attr_str .= "<input type='checkbox' id='".$checkboxid."' name='".$checkboxid."' value='".$checkbox_button_name."' ".$selected_value."></input><span class='checkmarkspan'></span></label>";
+	 	
 	}else{
+		//echo "tested";
 	   $checkbox_button_attr_str = '<p class="error_msg">Please add button shortcode name Attribute<p>';
 	} 
+
+	$count++;
 	return $checkbox_button_attr_str;	
 }
 add_shortcode('button', 'custom_checkbox_button');
@@ -69,13 +86,15 @@ add_shortcode('button', 'custom_checkbox_button');
  * Custom checkbox action to save checkbox details --Start
  */
 add_action( 'wp_ajax_custom_checkbox_action', 'custom_checkbox_action' );
+
 function custom_checkbox_action() {
-    global $wpdb,$wp_query;
+    global $wpdb,$wp_query;    
 	$checkboxname = $_REQUEST['checkboxname'];
 	$checkboxvalue = $_REQUEST['checkboxvalue'];
 	$current_page_id = $_REQUEST['page_id'];	
 	$usert_id = get_current_user_id();	
 	$current_date = date("Y-m-d h:i:s");
+    
     if($checkboxname && $checkboxvalue && $usert_id){
 		$table_name = $wpdb->prefix . 'custom_checkbox_details';		
 		$result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE checkboxname LIKE '%".$checkboxname."%' and pageid LIKE '%".$current_page_id."%' and userid LIKE '%".$usert_id."%'"); 		
@@ -103,7 +122,7 @@ function custom_checkbox_action() {
  function checkbox_select_display($page_id,$checkboxid,$usert_id){
 	global $wpdb;
 	$table_name = $wpdb->prefix . 'custom_checkbox_details';		
-	$result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE checkboxname LIKE '%".$checkboxid."%' and pageid LIKE '%".$current_page_id."%' and userid LIKE '%".$usert_id."%'");
+	$result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE checkboxname LIKE '%".$checkboxid."%' and pageid LIKE '%".$page_id."%' and userid LIKE '%".$usert_id."%'");
 	if($wpdb->num_rows > 0 ){		
 		$checkboxvalue = $result[0]->checkboxvalue;
 		$checkbox_str = '';
@@ -116,46 +135,3 @@ function custom_checkbox_action() {
 /*
  * Login user selected checkbox display  --End
  */
-
-function checkboxname_check($checkbox_name,$page_id){
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'custom_master_checkbox';		
-	 $result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE checkboxname LIKE '%".$checkbox_name."%' and pageid LIKE '%".$page_id."%'");
-	$checkbox_str = '';
-	$current_date = date("Y-m-d h:i:s");	
-	echo $wpdb->num_rows;
-	if($wpdb->num_rows > 0 ){	
-	     echo $checkbox_check = $result[0]->display;
-		  if($checkbox_check == 'yes'){
-			   $checkbox_str = 'exit';
-			  /* $wpdb->query( $wpdb->prepare("UPDATE $table_name 
-                SET checkboxvalue = %s , updated_date = %s
-             WHERE checkboxname = %s and pageid=%s",$checkboxvalue,$current_date,$checkboxname,$current_page_id)
-           );*/
-			  $wpdb->query( $wpdb->prepare("UPDATE $table_name 
-                SET display = %s WHERE checkboxname = %s and pageid=%s",'no',$checkbox_name,$page_id));
-				
-		  }else{
-			 $checkbox_str = 'new';
-			 $wpdb->query( $wpdb->prepare("UPDATE $table_name 
-                SET display = %s 
-             WHERE checkboxname = %s and pageid=%s",'yes',$checkbox_name,$page_id));
-          }   			  
-		  //$checkbox_str = 'exit';
-         /*if($wpdb->num_rows == 1){
-			$checkbox_str = 'new';
-		 }else{			 
-		  
-		 }*/
-	}else{
-		 $checkbox_str = 'new';
-		 $wpdb->insert($table_name, array(	        
-				'checkboxname' => $checkbox_name,				
-				'pageid' => $page_id,
-				'created_date' => $current_date,
-				'display' => 'yes'
-			));
-	}	
-	return $checkbox_str;	
-}	
-
